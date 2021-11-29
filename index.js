299210de3415237cf5c4cee7357cf0936c952f30
Dakota Castle <barrettcastle@gmail.com>
2021-11-29 10:55:08 -0500

    commit tree 2cd2ae5e48773979117619ee8b3f05575b292abb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"]
+
+function findMatching(drivers, name) {
+    const match = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+    return match;
+}
+
+function fuzzyMatch(drivers, letter) {
+    const letters = drivers.filter(driver => driver[0].toUpperCase() === letter[0].toUpperCase());
+    return letters;
+}
+
+function matchName(drivers, string) {
+    const strings = drivers.filter(driver => driver.name.toUpperCase() === string.toUpperCase());
+    return strings;
+}
